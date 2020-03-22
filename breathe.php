c340d78f771bed244066c99f9e9fa5db45adfdf4
Niinajhe <niina.heinonen2@metropolia.fi>
2020-03-22 20:47:04 +0200

    Hengitysharjoitus

---
+++ breathe.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fi-FI">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" media="screen and (max-width: 500px)" href="css/smallscreen.css">
+    <link rel="stylesheet" media="screen and (min-width: 500px)" href="css/widescreen.css">
+    <title>Breathe</title>
+</head>
+
+<body>
+    <h3>Hengitysharjoitus</h3>
+
+    <div>
+        1 min
+    </div>
+
+    <div>
+        2 min
+    </div>
+
+    <div>
+        5 min
+    </div>
+
+    <h3>Rentoutumisharjoitus</h3>
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/loLZIfXzReg" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>
+    
+    <div class="footer">
+        <p>footer</p>
+    </div>
+    
+    </body>
+    </html>
+
