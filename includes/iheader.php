1a99c34f95bc6709f8e7dbd38db21ff8776d7cb5
unknown <janina.saarinen@metropolia.fi>
2020-03-24 11:08:46 +0200

    iheader.php kansioon includes

---
+++ includes/iheader.php
@@ -0,0 +1,14 @@
+<?php
+include_once("config/chttps.php");
+?>
+<!DOCTYPE html>
+<html lang="fi-FI">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" media="screen and (max-width: 500px)" href="css/smallscreen.css">
+    <link rel="stylesheet" media="screen and (min-width: 500px)" href="css/widescreen.css">
+    <title>Index</title>
+</head>
+<body>
