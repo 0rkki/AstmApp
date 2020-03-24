1f77df0c5b21a55d85d7cec90c1a2aeea9f5d72a
unknown <janina.saarinen@metropolia.fi>
2020-03-24 11:07:01 +0200

    siirretty header omalle sivulle iheader.php

--- index.php
+++ index.php
@@ -1,22 +1,10 @@
 <?php
-include_once("config/chttps.php");
-?>
-<!DOCTYPE html>
-<html lang="fi-FI">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" media="screen and (max-width: 500px)" href="css/smallscreen.css">
-    <link rel="stylesheet" media="screen and (min-width: 500px)" href="css/widescreen.css">
-    <title>Index</title>
-</head>
-<body>
+include("includes/iheader.php");?>
 
 <!--header, navigointi -->
-<div id="header">
+<div id="topnav">
 		<nav>
-            <div class="topnav">
+            <div class="topnavRight">
 			<ul>
 				<li><a href="#">Log out</a></li>
 				<li><a href="#">About</a></li>
@@ -24,7 +12,7 @@ include_once("config/chttps.php");
             </ul>
             </div>
 		</nav>
-	</div>
+</div>
 <main>
 <div class="grid-box-container">
 
